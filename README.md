df85754324a030697ddb7668863a611e204d1e82
wessal <wessalalmansory@gmail.com>
2025-11-25 19:16:59 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Basic-school-data-management-system" 
---
+++ src/main/java/wessal/basicschool/BasicSchool.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package wessal.basicschool;
+
+/**
+ *
+ * @author wessal
+ */
+public class BasicSchool {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
