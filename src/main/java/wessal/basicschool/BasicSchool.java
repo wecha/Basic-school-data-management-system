0030949ece48c59927fe51319a38f41c767a3532
wessal <wessalalmansory@gmail.com>
2025-11-25 19:59:20 +0200

    second

--- src/main/java/wessal/basicschool/BasicSchool.java
+++ src/main/java/wessal/basicschool/BasicSchool.java
@@ -12,5 +12,7 @@ public class BasicSchool {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+         System.out.println("Hello World!");
+          System.out.println("Hello World!");
     }
 }
